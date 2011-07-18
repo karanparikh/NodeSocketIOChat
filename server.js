0d7c53537e1b51dc31dd070f3a96ae04252f8c46
Karan Parikh <karan.painkiller@gmail.com>
2011-07-17 19:49:01 -0700

    Added date stamp to messages.

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var io = require('socket.io').listen(8000);
 
 io.sockets.on('connection', function(socket) {
     console.log("Client connected!");
+
+    //set the username
+    socket.on('set_nickname', function(nickname) {
+        socket.nickname = nickname;
+        socket.broadcast.emit('new_user', nickname);
+    });
+
+    //message recvd from the client socket
     socket.on('message', function(data) {
+        var now = new Date();
+        var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        data = "<i>" + socket.nickname + "[" + time + "]:</i> " + data;
         console.log("Recvd: ", data);
         io.sockets.emit('chat', data);
     });
