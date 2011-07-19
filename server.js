f566f9f818ef64f0e3c8f4d5910d4a16756bded9
Karan Parikh <karan.painkiller@gmail.com>
2011-07-18 21:58:34 -0700

    complete server overhaul. Can now serve static files.

--- server.js
+++ server.js
@@ -1,4 +1,46 @@
-var io = require('socket.io').listen(8000);
+var http = require('http'),
+    path = require('path'),
+    fs   = require('fs'),
+    url  = require('url');
+
+var server = http.createServer(function(request, response){
+    //static file server
+    var uri = url.parse(request.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+    console.log("URI: " + uri);
+    console.log("Filename: " + filename);
+    path.exists(filename, function(exists) {
+        if(exists){
+            fs.stat(filename, function(err, stat) {
+                var rs;
+                if (err) {
+                    response.writeHead(500);
+                    response.end("Error reading the file");
+                }
+                if(stat.isDirectory()){
+                    response.writeHead(403);
+                    response.end("Cannot display directories!");
+                }
+                else{
+                    rs = fs.createReadStream(filename);
+                    response.writeHead(200);
+                    rs.pipe(response);
+                }
+            });
+        }
+        else{
+            response.writeHead(404);
+            response.end("404!");
+        }
+    });
+});
+
+server.listen(8000);
+console.log("Server started!");
+
+//attach socket.io to our http static file server
+var io = require('socket.io').listen(server);
+console.log("SocketIO started!");
 
 io.sockets.on('connection', function(socket) {
     console.log("Client connected!");
